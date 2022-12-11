5656a26b7617ef9caf950423fb6e90d8fc928008
David Speck <david@saduras.de>
2022-12-11 12:34:15 +0100

    Create TrackGenerator and spawn prefabs from track tile data

---
+++ ProcWheels/Assets/Scripts/TrackGenerator.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+enum TileType
+{
+    None,
+    StraightNS,
+    StraightEW,
+    CornerSE,
+    CornerSW,
+    CornerNE,
+    CornerNW,
+}
+
+public class TrackGenerator : MonoBehaviour
+{
+    public GameObject roadStraightPrefab;
+    public GameObject roadCornerSmallPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        // Start track
+        TileType[,] tiles = new TileType[,]{
+            {TileType.CornerSE, TileType.StraightEW, TileType.CornerSW },
+            {TileType.StraightNS, TileType.None, TileType.StraightNS },
+            {TileType.CornerNE, TileType.StraightEW, TileType.CornerNW },
+        };
+
+        // Modify track
+        GenerateTrack(tiles);
+
+        // Destroy previous track
+        foreach (Transform childTransform in this.transform)
+            GameObject.Destroy(childTransform.gameObject);
+
+        // Spawn new track
+        SpawnPrefabs(tiles);
+    }
+
+    void GenerateTrack(TileType[,] tiles)
+    {
+        // TODO do something cool here
+    }
+
+    void SpawnPrefabs(TileType[,] tiles)
+    {
+        float gridSize = 10.0f;
+
+        for (int z = 0; z < tiles.GetLength(0); z++)
+            for (int x = 0; x < tiles.GetLength(1); x++)
+            {
+                TileType tile = tiles[z, x];
+                GameObject road = null;
+                switch (tile)
+                {
+                    case TileType.None:
+                        break;
+                    case TileType.StraightNS:
+                        road = Instantiate(roadStraightPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.identity, this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    case TileType.StraightEW:
+                        road = Instantiate(roadStraightPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.Euler(0.0f, 90.0f, 0.0f), this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    case TileType.CornerSE:
+                        road = Instantiate(roadCornerSmallPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.identity, this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    case TileType.CornerSW:
+                        road = Instantiate(roadCornerSmallPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.Euler(0.0f, 90.0f, 0.0f), this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    case TileType.CornerNE:
+                        road = Instantiate(roadCornerSmallPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.Euler(0.0f, -90.0f, 0.0f), this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    case TileType.CornerNW:
+                        road = Instantiate(roadCornerSmallPrefab, new Vector3(x * gridSize, 0.0f, -z * gridSize), Quaternion.Euler(0.0f, 180.0f, 0.0f), this.transform);
+                        road.name = String.Format("{0} ({1}, {2})", tile, x, z);
+                        break;
+                    default:
+                        throw new ArgumentException(string.Format("Unsupported tile type {0}", tile));
+                }
+            }
+    }
+}
